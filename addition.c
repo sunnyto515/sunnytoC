9d4d88b081d33b6864e2c7c1f6ff497fd9f40ed2
sunnyto515 <yerimtoto305@gmail.com>
2021-01-12 15:26:55 +0700

    first commit

---
+++ addition.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// ดึง input & output
+
+
+void main(){
+//ส่วนที่ 2 ฟังก์ขั่นหลักของภาษาซ๊
+
+//----------------คำสั่ง---------------
+int num1, num2, total; 
+//ประกาศตัวแปร 1 2 และ คำตอบ
+
+printf("value of num1 is :");
+scanf("%d" ,&num1);
+
+printf("value of num2 is :");
+scanf("%d" ,&num2);   
+
+total = num1 + num2;
+printf("Result is %d",total);
+//----------------สิ้นสุดโปรแกรม--------
+}
+
---
+++ work1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    printf("Supanut Jangchudjai");
+    printf("otto");
+    printf("18");
+    printf("Girl gennertion redvelvet i*zone mamamoo");
+    printf("sleep");
+    printf("watching favor concert");
+
+}
